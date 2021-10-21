ff7db328b81c389ca14c16623b75aa3d87b05349
bogdan-kovalchuk <bogdan.kovalchuk@globallogic.com>
2021-10-21 15:50:38 +0300

    format main

--- src/main.cpp
+++ src/main.cpp
@@ -3,7 +3,8 @@
 #include "game.h"
 #include "renderer.h"
 
-int main() {
+int main()
+{
   constexpr std::size_t kFramesPerSecond{60};
   constexpr std::size_t kMsPerFrame{1000 / kFramesPerSecond};
   constexpr std::size_t kScreenWidth{640};
@@ -18,5 +19,6 @@ int main() {
   std::cout << "Game has terminated successfully!\n";
   std::cout << "Score: " << game.GetScore() << "\n";
   std::cout << "Size: " << game.GetSize() << "\n";
+  
   return 0;
 }
