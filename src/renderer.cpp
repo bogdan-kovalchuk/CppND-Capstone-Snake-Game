79ca08f19b6f2917133edbd4c78c9219090082a3
bogdan-kovalchuk <bogdan.kovalchuk@globallogic.com>
2021-11-05 10:07:36 +0200

    Adding an underscore to private members of the Renderer class.

--- src/renderer.cpp
+++ src/renderer.cpp
@@ -5,10 +5,10 @@
 Renderer::Renderer(const std::size_t screen_width,
                    const std::size_t screen_height,
                    const std::size_t grid_width, const std::size_t grid_height)
-    : screen_width(screen_width),
-      screen_height(screen_height),
-      grid_width(grid_width),
-      grid_height(grid_height),
+    : screen_width_(screen_width),
+      screen_height_(screen_height),
+      grid_width_(grid_width),
+      grid_height_(grid_height),
       sdl_window_(nullptr, SDL_DestroyWindow)
 {
   // Initialize SDL
@@ -20,8 +20,8 @@ Renderer::Renderer(const std::size_t screen_width,
 
   // Create Window
   sdl_window_.reset(SDL_CreateWindow("Snake Game", SDL_WINDOWPOS_CENTERED,
-                                     SDL_WINDOWPOS_CENTERED, screen_width,
-                                     screen_height, SDL_WINDOW_SHOWN));
+                                     SDL_WINDOWPOS_CENTERED, screen_width_,
+                                     screen_height_, SDL_WINDOW_SHOWN));
 
   if (nullptr == sdl_window_)
   {
@@ -30,8 +30,8 @@ Renderer::Renderer(const std::size_t screen_width,
   }
 
   // Create renderer
-  sdl_renderer = SDL_CreateRenderer(sdl_window_.get(), -1, SDL_RENDERER_ACCELERATED);
-  if (nullptr == sdl_renderer)
+  sdl_renderer_ = SDL_CreateRenderer(sdl_window_.get(), -1, SDL_RENDERER_ACCELERATED);
+  if (nullptr == sdl_renderer_)
   {
     std::cerr << "Renderer could not be created.\n";
     std::cerr << "SDL_Error: " << SDL_GetError() << "\n";
@@ -54,26 +54,26 @@ Renderer::~Renderer()
 void Renderer::Render(Snake const snake, SDL_Point const &food)
 {
   SDL_Rect block;
-  block.w = screen_width / grid_width;
-  block.h = screen_height / grid_height;
+  block.w = screen_width_ / grid_width_;
+  block.h = screen_height_ / grid_height_;
 
   // Clear screen
-  SDL_SetRenderDrawColor(sdl_renderer, 0x1E, 0x1E, 0x1E, 0xFF);
-  SDL_RenderClear(sdl_renderer);
+  SDL_SetRenderDrawColor(sdl_renderer_, 0x1E, 0x1E, 0x1E, 0xFF);
+  SDL_RenderClear(sdl_renderer_);
 
   // Render food
-  SDL_SetRenderDrawColor(sdl_renderer, 0xFF, 0xCC, 0x00, 0xFF);
+  SDL_SetRenderDrawColor(sdl_renderer_, 0xFF, 0xCC, 0x00, 0xFF);
   block.x = food.x * block.w;
   block.y = food.y * block.h;
-  SDL_RenderFillRect(sdl_renderer, &block);
+  SDL_RenderFillRect(sdl_renderer_, &block);
 
   // Render snake's body
-  SDL_SetRenderDrawColor(sdl_renderer, 0xFF, 0xFF, 0xFF, 0xFF);
+  SDL_SetRenderDrawColor(sdl_renderer_, 0xFF, 0xFF, 0xFF, 0xFF);
   for (SDL_Point const &point : snake.body)
   {
     block.x = point.x * block.w;
     block.y = point.y * block.h;
-    SDL_RenderFillRect(sdl_renderer, &block);
+    SDL_RenderFillRect(sdl_renderer_, &block);
   }
 
   // Render snake's head
@@ -81,16 +81,16 @@ void Renderer::Render(Snake const snake, SDL_Point const &food)
   block.y = static_cast<int>(snake.head_y) * block.h;
   if (snake.alive)
   {
-    SDL_SetRenderDrawColor(sdl_renderer, 0x00, 0x7A, 0xCC, 0xFF);
+    SDL_SetRenderDrawColor(sdl_renderer_, 0x00, 0x7A, 0xCC, 0xFF);
   }
   else
   {
-    SDL_SetRenderDrawColor(sdl_renderer, 0xFF, 0x00, 0x00, 0xFF);
+    SDL_SetRenderDrawColor(sdl_renderer_, 0xFF, 0x00, 0x00, 0xFF);
   }
-  SDL_RenderFillRect(sdl_renderer, &block);
+  SDL_RenderFillRect(sdl_renderer_, &block);
 
   // Update Screen
-  SDL_RenderPresent(sdl_renderer);
+  SDL_RenderPresent(sdl_renderer_);
 }
 
 void Renderer::UpdateWindowTitle(int score, int fps)
--- src/renderer.h
+++ src/renderer.h
@@ -19,12 +19,12 @@ public:
 
 private:
   std::unique_ptr<SDL_Window, decltype(&SDL_DestroyWindow)> sdl_window_;
-  SDL_Renderer *sdl_renderer;
+  SDL_Renderer *sdl_renderer_;
 
-  const std::size_t screen_width;
-  const std::size_t screen_height;
-  const std::size_t grid_width;
-  const std::size_t grid_height;
+  const std::size_t screen_width_;
+  const std::size_t screen_height_;
+  const std::size_t grid_width_;
+  const std::size_t grid_height_;
 };
 
 #endif
