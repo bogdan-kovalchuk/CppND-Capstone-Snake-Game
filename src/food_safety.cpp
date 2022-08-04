8c6564ff0b5cdacea390af247d1920a908a1a87a
bogdan-kovalchuk <bogdankovalchuk@gmail.com>
2022-08-04 12:00:00 +0300

    Add food placement safety helpers

---
+++ src/food_safety.cpp
@@ -0,0 +1,50 @@
+#include "food_safety.h"
+
+bool IsFoodOnGrid(GridPoint food, int grid_width, int grid_height)
+{
+    return IsOnGrid(food, grid_width, grid_height);
+}
+
+bool IsFoodClearOfSnake(GridPoint food, const std::vector<GridPoint> &snake_body)
+{
+    for (const auto &seg : snake_body)
+    {
+        if (food.x == seg.x && food.y == seg.y)
+        {
+            return false;
+        }
+    }
+    return true;
+}
+
+bool IsFoodSafe(GridPoint food, int grid_width, int grid_height,
+               const std::vector<GridPoint> &snake_body)
+{
+    if (!IsFoodOnGrid(food, grid_width, grid_height))
+    {
+        return false;
+    }
+    return IsFoodClearOfSnake(food, snake_body);
+}
+
+int CountUnsafePositions(int grid_width, int grid_height,
+                         const std::vector<GridPoint> &snake_body)
+{
+    if (!IsValidGrid(grid_width, grid_height))
+    {
+        return 0;
+    }
+    int count = 0;
+    for (int y = 0; y < grid_height; ++y)
+    {
+        for (int x = 0; x < grid_width; ++x)
+        {
+            GridPoint p{x, y};
+            if (!IsFoodClearOfSnake(p, snake_body))
+            {
+                count++;
+            }
+        }
+    }
+    return count;
+}
---
+++ src/food_safety.h
@@ -0,0 +1,17 @@
+#ifndef FOOD_SAFETY_H
+#define FOOD_SAFETY_H
+
+#include "collision.h"
+#include <vector>
+
+bool IsFoodOnGrid(GridPoint food, int grid_width, int grid_height);
+
+bool IsFoodClearOfSnake(GridPoint food, const std::vector<GridPoint> &snake_body);
+
+bool IsFoodSafe(GridPoint food, int grid_width, int grid_height,
+               const std::vector<GridPoint> &snake_body);
+
+int CountUnsafePositions(int grid_width, int grid_height,
+                         const std::vector<GridPoint> &snake_body);
+
+#endif
